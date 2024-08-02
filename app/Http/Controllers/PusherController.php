9d06a501cd141c8922b63359c7fa5878108fa4a1
Christopher Jr. <christopherjrcabatbat016@gmail.com>
2024-08-02 23:01:48 +0800

    nalagay na lahat

--- app/Http/Controllers/PusherController.php
+++ app/Http/Controllers/PusherController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\PusherBroadcast;
 use Illuminate\Http\Request;
 
 class PusherController extends Controller
 {
-    //
+
+    public function index() {
+        return view(view: 'index');
+    }
+
+    public function broadcast(Request $request) {
+
+        broadcast(new PusherBroadcast($request->get(key: 'message')))->toOthers();
+
+        return view(view: 'broadcast', ['message'=> $request->get(key: 'message')]);
+    }
+
+    public function receive(Request $request) {
+        return view(view: 'receive', ['message'=> $request->get(key: 'message')]);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get( uri: '/', action: 'App\Http\Controllers\PusherController@index');
+Route::post( uri: '/broadcast', action: 'App\Http\Controllers\PusherController@broadcast');
+Route::post( uri: '/receive', action: 'App\Http\Controllers\PusherController@receive');
